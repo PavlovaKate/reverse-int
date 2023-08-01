1f1d33fd6e910f7fc9ef8276d3cbca8972db250d
PavlovaKate <kv.pavlova@mail.ru>
2023-08-01 13:36:37 +0300

    feat: implement reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0) {
+  n = String(n);
+  n = n.split("").reverse().join("");
+  return +n;} else {
+  n = -n;
+  n = String(n);
+  n = n.split("").reverse().join("");
+  return +n;
+  }
 }
